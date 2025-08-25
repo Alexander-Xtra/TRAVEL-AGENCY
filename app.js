1dba1d17dd6a330394c1a7d35a8abdcaa9b787e3
Alexander-Xtra <obikwelukamsy644@gmail.com>
2025-08-25 15:15:42 +0100

    changes in the pages

--- app.js
+++ app.js
@@ -94,5 +94,5 @@ function logout() {
   localStorage.clear();
   alert("You have been logged out!");
 
-  window.location.href = "../login page/login.html";
+  window.location.href = "../sing-up page/signup.html";
 }
